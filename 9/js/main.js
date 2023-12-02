8b3f8382aa24d7aca6be4dff86fe114b7554c3d7
Keks <keks@htmlacademy.ru>
2023-12-02 23:55:08 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,5 @@
+//  точка входа
+import {arrayPublications} from './data.js'; // массив публикаций
+import {createElements} from './miniatures.js'; // функция создания миниатюр
+
+createElements(arrayPublications); // создаем миниатюры по массиву публикаций
